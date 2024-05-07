39b22f517b2c1ccf46236982f520cf53ec20dbf9
JagadeeshAleti <jagadeesh.aleti955@gmail.com>
2024-05-07 18:33:26 +0530

    Creating witUserData HOC to useUserData custom React hook

--- components/UserList/index.js
+++ components/UserList/index.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import axios from 'axios';
+import useUserData from '../hooks/useUserData';
 
 const Table = styled.table(() => ({
   width: '100%',
@@ -35,87 +35,15 @@ const columnFields = [
   { value: 'website', label: 'Website' },
 ];
 
-const withUserData = (WrappedComponent) => {
-  return () => {
-    const [users, setUsers] = useState([]);
-    const [filteredUsers, setFilteredUsers] = useState([]);
-    const [searchName, setSearchName] = useState('');
-    const [searchEmail, setSearchEmail] = useState('');
-    const [sortColumn, setSortColumn] = useState(columnFields[0].value);
-    const [sortDirection, setSortDirection] = useState('asc');
+const UserList = () => {
+  const {
+    users,
+    handleOnSearch,
+    handleSort,
+    sortColumn,
+    sortDirection,
+  } = useUserData(columnFields);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        const { data } = await axios.get('/api/v1/users');
-        setUsers(data);
-        setFilteredUsers(data);
-      };
-      fetchData();
-    }, []);
-
-    useEffect(() => {
-      let filteredUsers = users.filter(
-        (user) =>
-          user.name.toLowerCase().includes(searchName.toLowerCase()) &&
-          user.email.toLowerCase().includes(searchEmail.toLowerCase())
-      );
-
-      if (sortColumn) {
-        filteredUsers.sort((a, b) => {
-          const x = a[sortColumn];
-          const y = b[sortColumn];
-          if (x < y) return sortDirection === 'asc' ? -1 : 1;
-          if (x > y) return sortDirection === 'asc' ? 1 : -1;
-          return 0;
-        });
-      }
-
-      setFilteredUsers(filteredUsers);
-    }, [searchName, searchEmail, users, sortColumn, sortDirection]);
-
-    const handleOnSearch = (event) => {
-      const { name, value } = event.target;
-      if (name === 'name') {
-        setSearchName(value);
-      } else if (name === 'email') {
-        setSearchEmail(value);
-      } else {
-        throw new Error('Unknown search element');
-      }
-    };
-
-    const handleSort = (column) => {
-      if (sortColumn === column) {
-        setSortDirection((prevSortDirection) =>
-          prevSortDirection === 'asc' ? 'desc' : 'asc'
-        );
-      } else {
-        setSortColumn(column);
-        setSortDirection('asc');
-      }
-    };
-
-    return (
-      <WrappedComponent
-        users={filteredUsers}
-        columnFields={columnFields}
-        handleOnSearch={handleOnSearch}
-        handleSort={handleSort}
-        sortColumn={sortColumn}
-        sortDirection={sortDirection}
-      />
-    );
-  };
-};
-
-const UserList = ({
-  users,
-  columnFields,
-  handleOnSearch,
-  handleSort,
-  sortColumn,
-  sortDirection,
-}) => {
   return (
     <div>
       <Table>
@@ -164,4 +92,4 @@ const UserList = ({
   );
 };
 
-export default withUserData(UserList);
+export default UserList;
